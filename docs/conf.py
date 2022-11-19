d7c18b793fa2a8be2c727d02282c1438dfaa8075
Sunny Patel <contact.sunny2812@gmail.com>
2022-11-20 03:47:24 +0530

    updated docs requirements

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'django_zappa_event_schedular'
+copyright = '2022, Tushar Patel'
+author = 'Tushar Patel'
+release = '5-beta'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,7 +1,20 @@
-==============================
-Django Zappa Event Schedular
-==============================
-Django Zappa Event Schedular
+.. django_zappa_event_schedular documentation master file, created by
+   sphinx-quickstart on Sun Nov 20 03:46:48 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
-Quick start
-------------
+Welcome to django_zappa_event_schedular's documentation!
+========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
